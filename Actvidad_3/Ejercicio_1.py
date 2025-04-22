ae1253d60ca15e280a36a16d2e6d5ff0a1571b51
mattiasrortega08 <mattiasrmo@gmail.com>
2025-04-22 07:59:36 -0600

    ok

--- Actvidad_3/Ejercicio_1.py
+++ Actvidad_3/Ejercicio_1.py
@@ -1,14 +1,15 @@
-calificacion1 = float(input("Ingrese la calificación 1: ")) 
-calificacion2 = float(input("Ingrese la calificación 2: "))
-calificacion3 = float(input("Ingrese la calificación 3: "))
+calificacion1 = int(input("Ingrese la calificación 1: ")) 
+calificacion2 = int(input("Ingrese la calificación 2: "))
+calificacion3 = int(input("Ingrese la calificación 3: "))
 suma = calificacion1 + calificacion2 + calificacion3
 promedio = suma / 3
 
-print(f"""Calificacion 1: {calificacion1})
-Calificacion 2: {calificacion2}
-Calificacion 3: {calificacion3}
-Promedio: {promedio:.0f}""")
+print(f"""Calificacion 1: {calificacion1:>3}
+ Calificacion 2: {calificacion2:>3}
+Calificacion 3: {calificacion3:>3}
+{promedio:<15} {promedio:>3.0f}""")
 if promedio >= 70:
     print("Aprobado")
 else:
     print("Reprobado")
+print("hola")
