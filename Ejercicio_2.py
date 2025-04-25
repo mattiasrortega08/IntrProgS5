55fb596423473081e7b9f4b488908d083bdd1a46
mattiasrortega08 <mattiasrmo@gmail.com>
2025-04-25 07:35:36 -0600

    Delete Ejercicio_2.py

--- Ejercicio_2.py
+++
@@ -1,9 +0,0 @@
-cantidad_inicial_inventario = int(input("Ingrese la cantidad inicial de inventario: "))
-cantidad_productos_recibidos = int(input("Ingrese la cantidad de productos recibidos: "))
-cantidad_inicial_vendido = int(input("Ingrese la cantidad inicial vendida: "))
-
-suma = cantidad_inicial_inventario + cantidad_productos_recibidos
-inventario_actual = suma - cantidad_inicial_vendido
-
-print(f"Cantidad inicial de inventario: {cantidad_inicial_inventario:>3}")
-print(f"Cantidad de productos recibidos: {cantidad_productos_recibidos:>3}")
